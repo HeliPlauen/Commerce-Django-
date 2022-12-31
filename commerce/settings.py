c09ceeadd15ee4af29b25b2c097c156278640172
HeliPlauen <Oleg.Shcherbatiouk@gmail.com>
2022-12-31 13:44:00 +0200

    6-th commit: changed settings.py (STATIC_ROOT)

--- commerce/settings.py
+++ commerce/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# !!!!! I`m trying to write down the static root !!!!!!
+#STATIC_ROOT = Path(BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
